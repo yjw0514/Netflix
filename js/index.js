f7a606a1bddba75187478b49ae5c3f6b51b5c2fa
yjw0514 <76725372+yjw0514@users.noreply.github.com>
2021-03-30 19:06:46 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function changeBg() {
+
+var imgNumPrev = 0;
+var imgNum = Math.floor(Math.random() * 10);
+
+// if (imgNum == imgNumPrev) {
+//     imgNum = Math.floor(Math.random() * 10);
+//     if (imgNum == imgNumPrev) {
+//         imgNum = Math.floor(Math.random() * 10);
+//     }
+// }
+
+while ( imgNum == imgNumPrev ) {
+    imgNum = Math.floor(Math.random() * 10);
+    break;
+}
+
+document.getElementById('hero_bg').style = "background-image: url(../img/poster_" + imgNum + ".jpg); "
+
+document.getElementById('hero_bg').classList.remove('effect_fade_in');
+void document.getElementById('hero_bg').offsetWidth; //undefined로 바꿔버림
+document.getElementById('hero_bg').classList.add('effect_fade_in');
+
+imgNumPrev = imgNum;
+
+}
+
+var changeHero = setInterval(changeBg, 3000);
