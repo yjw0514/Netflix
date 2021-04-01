c231b466ce8e57c277685ce5c208ee723d0ed4e0
yjw0514 <76725372+yjw0514@users.noreply.github.com>
2021-04-01 18:36:19 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -25,7 +25,39 @@ imgNumPrev = imgNum;
 
 }
 
-var changeHero = setInterval(changeBg, 3000);
+var changeHero = setInterval(changeBg, 5000);
 
 
-// story_card animation
+// faq
+let faqQ = document.querySelectorAll('.faq_q');
+let faqsvg = document.querySelectorAll('.faq_q_svg');
+
+let faqopenstate = [];
+
+for (let i=0; i<faqQ.length; i++) {
+    faqopenstate[i] = false;
+}
+
+function faqOpen(target) {
+
+    for (let i=0; i<faqQ.length; i++ ) {
+        if (i != target) {
+            faqQ[i].style = "height: 80px;";
+            faqsvg[i].style = "transform: rotate(45deg);";
+            faqopenstate[i] = false;
+        }
+    }
+
+    if (faqopenstate[target]) {
+        faqQ[target].style = "height: 80px;";
+        faqsvg[target].style = "transform: rotate(45deg);";
+        faqopenstate[target] = false;
+    }
+
+    else {
+        faqQ[target].style = "max-height: 500px;";
+        faqsvg[target].style = "transform: rotate(90deg);";
+        faqopenstate[target] = true;
+    }
+}
+
